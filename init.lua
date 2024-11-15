906ec9974d5f881448c513f02cf03b0d41c99a81
Stel Clementine <dev@stelclementine.com>
2024-11-14 20:13:50 -0800

    Add mark functionality

--- init.lua
+++ init.lua
@@ -1,9 +1,25 @@
 local function fail(s, ...) ya.notify { title = "bunny.yazi", content = string.format(s, ...), timeout = 3, level = "error" } end
 local function info(s, ...) ya.notify { title = "bunny.yazi", content = string.format(s, ...), timeout = 3, level = "info" } end
 
-local get_state_attr = ya.sync(function(state, attr)
+local get_state = ya.sync(function(state, attr)
   return state[attr]
 end)
+
+local set_state = ya.sync(function(state, attr, value)
+  state[attr] = value
+end)
+
+local get_cwd = ya.sync(function(state)
+  -- https://github.com/sxyazi/yazi/blob/main/yazi-plugin/preset/plugins/zoxide.lua
+  return tostring(cx.active.current.cwd)
+end)
+
+local add_mark_to_hops = function(hops)
+  local mark = get_state("mark")
+  if mark and mark ~= "" then
+    table.insert(hops, { key = "<space>", tag = "marked", path = mark, })
+  end
+end
 
 local validate_options = function(options)
   local hops, fuzzy_cmd, notify = options.hops, options.fuzzy_cmd, options.notify
@@ -93,9 +109,22 @@ local select_key = function(hops)
   return { tag = selection.desc, path = selection.path }
 end
 
+local mark = function(notify)
+  local cwd = get_cwd()
+  if cwd then
+    set_state("mark", cwd)
+    if notify then
+      info("Marked current dir")
+    end
+  else
+    fail("Failed to set mark")
+  end
+end
+
 local hop = function(selected_hop, notify)
   if selected_hop and selected_hop.path then
     ya.manager_emit("cd", { selected_hop.path })
+    -- TODO: Better way to verify hop was successful?
     if notify then
       info('Hopped to "' .. selected_hop.tag .. '"')
     end
@@ -125,17 +154,25 @@ return {
     state.hops = hops
   end,
   entry = function(_self, args)
-    local init_error = get_state_attr("init_error")
+    local init_error = get_state("init_error")
     if init_error then
       fail(init_error)
       return
     end
-    local fuzzy_cmd, hops, notify = get_state_attr("fuzzy_cmd"), get_state_attr("hops"), get_state_attr("notify")
-    local select_by = args[1] or "key"
-    if select_by == "key" then
+    local fuzzy_cmd, hops, notify = get_state("fuzzy_cmd"), get_state("hops"), get_state("notify")
+    add_mark_to_hops(hops)
+    local cmd = args[1]
+    if not cmd or cmd == "" then
+      cmd = "hop"
+    end
+    if cmd == "hop" then
       hop(select_key(hops), notify)
-    elseif select_by == "fuzzy" then
+    elseif cmd == "hop_fuzzy" then
       hop(select_fuzzy(hops, fuzzy_cmd), notify)
+    elseif cmd == "mark" then
+      mark(notify)
+    else
+      fail("Unrecognized command \"" .. cmd .. "\" ")
     end
   end,
 }
